0044c5d7c8c7fd3fee62ee5777afe3dade05f185
unknown <mvassvik@gmail.com>
2018-03-06 20:08:19 +0100

    Full skeleton

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # OpenGL-Skeleton
+
+A minimal OpenGL 3.3 example using `GLFW` for window management and input handling, and `glad` for OpenGL function pointer loading.
+
+In Linux, install `libglfw3-dev` or the equivalent package for your distro, or build it from source (https://github.com/glfw/glfw)
+
+In Windows, get the binaries (http://www.glfw.org/download.html), or build it from source (https://github.com/glfw/glfw).
+
+In Linux, build with `gcc main.c -Iinclude -lglfw3 -lm -ldl` if using the system install of `GLFW`, or `gcc main.c -Iinclude -Llib -lglfw3 -lm -ldl` if you built yourself.
+
+In Windows, build with `cl main.c /MT /include lib\glfw3.lib gdi32.lib user32.lib shell32.lib libcmt.lib`. 
--- main.c
+++ main.c
@@ -4,8 +4,66 @@
 #include <glad/glad.c>
 #include <GLFW/glfw3.h>
 
-int main() {
-	glfwInit(); // just test that GLFW works
+void error_callback(int error, const char* description) {
+    fprintf(stderr, "Error %d: %s\n", error, description);
+}
 
-	return 0;
+int main() {
+    //
+    glfwSetErrorCallback(error_callback);
+
+    //
+    if (!glfwInit()) {
+        return 1;
+    }
+
+    //
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "Title", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return 2;
+    }
+
+    //
+    glfwMakeContextCurrent(window);
+
+    //
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        printf("Error: Could not load OpenGL functions");
+        glfwTerminate();
+        return 3;
+    }
+
+    //
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    while (!glfwWindowShouldClose(window)) {
+        //
+        glfwPollEvents();
+
+        //
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE)) glfwSetWindowShouldClose(window, GLFW_TRUE);
+
+        //
+        if (glfwGetKey(window, GLFW_KEY_1)) glClearColor(1.0, 0.7, 0.4, 1.0);
+        if (glfwGetKey(window, GLFW_KEY_2)) glClearColor(1.0, 0.4, 0.7, 1.0);
+        if (glfwGetKey(window, GLFW_KEY_3)) glClearColor(0.7, 1.0, 0.4, 1.0);
+        if (glfwGetKey(window, GLFW_KEY_4)) glClearColor(0.4, 1.0, 0.7, 1.0);
+        if (glfwGetKey(window, GLFW_KEY_5)) glClearColor(0.7, 0.4, 1.0, 1.0);
+        if (glfwGetKey(window, GLFW_KEY_6)) glClearColor(0.4, 0.7, 1.0, 1.0);
+        if (glfwGetKey(window, GLFW_KEY_0)) glClearColor(1.0, 1.0, 1.0, 1.0);
+        
+        //
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //
+        glfwSwapBuffers(window);
+    }
+
+    //
+    glfwTerminate();
+
+    return 0;
 }
